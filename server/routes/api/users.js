d32004a96015ce9a800a9f881e02f983a8756e46
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-06-10 01:20:20 -0500

    Change getUser to getUserInfo end point using token instead of params

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const { authenticate, verifyPermission } = require("../../utils/permissions");
 
-const { createUser, deleteUser, getUsers, getUser, loginUser } = require("../../controllers/users");
+const { createUser, deleteUser, getUsers, getUserInfo, loginUser } = require("../../controllers/users");
 
 router.get("/", getUsers);
 
 router.post("/login", loginUser);
 
 router.post("/", createUser);
-router.get("/:id", [authenticate, verifyPermission], getUser);
+router.get("/userInfo", authenticate, getUserInfo);
 router.delete("/:id", [authenticate, verifyPermission], deleteUser);
 
 
